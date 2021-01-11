d2941d44f9586c741fe8448b28fa287109544f44
Yash3730 <73027606+Yash3730@users.noreply.github.com>
2021-01-11 16:50:26 +0530

    Add files via upload

---
+++ boll.js
@@ -0,0 +1,33 @@
+class Boll{
+constructor (x,y,radius){
+    var options={
+        density:1,
+        frictionAir:0.005
+    }
+    this.body=Bodies.circle(x,y,radius,options)
+    this.radius=radius
+    World.add(world,this.body);
+
+}
+display(){
+    push ();
+    fill ("black")
+    ellipseMode(RADIUS)
+    ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius)
+pop ();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
